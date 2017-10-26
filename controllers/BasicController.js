8bed195d3677ba1142b77e36b746254367686001
Marcus Smith <marcussmith0711@gmail.com>
2017-10-25 21:03:33 -0500

    added GET /basic-card controller logic

--- controllers/BasicController.js
+++ controllers/BasicController.js
@@ -2,6 +2,12 @@ const _ = require("lodash");
 
 const BasicCards = require("./../models/BasicCards");
 
+exports.getAllGroups = (req, res) => {
+    BasicCards.find({}).then((cards) => {
+        res.send({cards});
+    }).catch(e => res.status(400).send());
+}
+
 exports.createCards = (req, res) => {
     let body = _.pick(req.body, ["title", "description"]);
     let newCards = new BasicCards(body);
