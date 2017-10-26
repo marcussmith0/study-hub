26a2dbe4b0a2c0b916fba2ecb15abe8e5af74c22
Marcus Smith <marcussmith0711@gmail.com>
2017-10-25 19:22:32 -0500

    added test packages

--- controllers/BasicController.js
+++ controllers/BasicController.js
@@ -5,4 +5,8 @@ const BasicCards = require("./../models/BasicCards");
 exports.createCards = (req, res) => {
     let body = _.pick(req.body, ["title", "description"]);
     let newCards = new BasicCards(body);
+
+    newCards.save().then((cards) => {
+        res.send({cards});
+    }).catch(e => res.status(400).send());
 }
