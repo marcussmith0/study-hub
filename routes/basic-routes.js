c1148ccffa2b4937a6e62c184cce5180f498ce3d
Marcus Smith <marcussmith0711@gmail.com>
2017-10-25 21:00:41 -0500

    added GET /basic-card

--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -1,6 +1,6 @@
 const BasicController = require("./../controllers/BasicController");
 
 module.exports = (app) => {
-    
+    app.get("/basic-card", BasicController.getAllGroups);
     app.post("/basic-card", BasicController.createCards);
 }
