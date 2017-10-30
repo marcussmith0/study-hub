fa2cb9bcf8804d848147add04aae0d8dce76606d
Marcus Smith <marcussmith0711@gmail.com>
2017-10-29 23:08:36 -0500

    added the DELETE /card-basic/:id route working

--- controllers/BasicController.js
+++ controllers/BasicController.js
@@ -71,6 +71,7 @@ exports.getSingleCard = (req, res) => {
     if(!ObjectID.isValid(id)) return res.status(400).send();
 
     CardBasic.findById(id).then((card) => {
+        if(!card) res.status(404).send();        
         res.send({card});
     }).catch(e => res.status(400).send());
 }
@@ -80,6 +81,20 @@ exports.patchCard = (req, res) => {
     if(!ObjectID.isValid(id)) return res.status(400).send();
 
     CardBasic.findByIdAndUpdate(id, {$set: req.body}, { new: true }).then((card) => {
+        if(!card) res.status(404).send();
         res.send({card});
     }).catch(e => res.status(400).send());
+}
+
+exports.deleteSingle = (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)) return res.status(400).send();
+
+    CardBasic.findByIdAndRemove(id).then((card) => {
+        if(!card) return res.status(404).send();
+        return BasicCards.findOneAndUpdate({cards: {$in: [card._id]}}, { $pull: { cards: card._id }}, {new: true});
+    }).then((group) => {
+        if(!group) return res.status(404).send();
+        res.send(group);
+    }).catch(e => res.status(400).send(e));
 }
