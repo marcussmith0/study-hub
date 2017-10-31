43224c7d8adfe8e3d0552a851bbd666958d02f1a
Marcus Smith <marcussmith0711@gmail.com>
2017-10-31 11:27:56 -0500

    added test cases for the GET routes

--- test/server/clozeRoutes.test.js
+++ test/server/clozeRoutes.test.js
@@ -5,8 +5,19 @@ const { ObjectId } = require("mongodb");
 const { app } = require("./../../server");
 const ClozeCard = require("./../../models/cloze/ClozeCard");
 
+const IDone = new ObjectId();
+const IDtwo = new ObjectId();
+
+
+const cards = [
+    {_id: IDone, title: "title one", description: "description one",  createdAt: new Date().getTime()},
+    {_id: IDtwo, title: "title two", description: "description two", createdAt: new Date().getTime()}
+]
+
 beforeEach((done) => {
-    ClozeCard.remove({}).then(() => done());
+    return ClozeCard.remove({}).then(() => {
+        return ClozeCard.insertMany(cards);  
+    }).then(() => done());
 });
 
 describe("POST /cloze-card", () => {
@@ -27,9 +38,9 @@ describe("POST /cloze-card", () => {
                 if (err) return done(err);
 
                 ClozeCard.find({}).then((result) => {
-                    expect(result.length).toBe(1);
+                    expect(result.length).toBe(3);
                     done();
-                })
+                }).catch(e => done(e));
             });
     });
 
@@ -42,5 +53,43 @@ describe("POST /cloze-card", () => {
             .send({title, description})
             .expect(400)
             .end(done);
+    });
+});
+
+describe("GET /cloze-card", () => {
+    it("should return all cards", (done) => {
+        request(app)
+            .get("/cloze-card")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.cards.length).toBe(2);
+            })
+            .end(done);
+    });
+});
+
+describe("GET /cloze-card/:id", () => {
+    
+    it("should return the correct card", (done) => {
+        let id = cards[0]._id.toHexString();
+        request(app)
+            .get(`/cloze-card/${id}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.group._id).toBe(id);
+                expect(res.body.group.title).toBe("title one");
+                expect(res.body.group.description).toBe("description one");
+                expect(res.body.group.createdAt).toBeA("number");
+            })
+            .end(done);
+    });
+
+    it("should return a 400 error", (done) => {
+        let id = cards[0]._id.toHexString() + "sdlf";
+
+        request(app)
+        .get(`/cloze-card/${id}`)
+        .expect(400)
+        .end(done);  
     });
 });
