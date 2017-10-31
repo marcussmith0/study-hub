fa93d3f58c4079c972423d0275ae61cf8f529c8c
Marcus Smith <marcussmith0711@gmail.com>
2017-10-31 04:51:12 -0500

    what did i change

--- models/cloze/Cloze.js
+++ models/cloze/Cloze.js
@@ -1,8 +1,19 @@
-const mongoose = requrie("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ClozeSchema = new Schema({
-
+    fullText : {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    word: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 1
+    }
 });
 
 module.exports = mongoose.model("Cloze", ClozeSchema);
